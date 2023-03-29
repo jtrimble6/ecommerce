30c1f03eefd6e1e54051f31c3adbba3ee2af66ac
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2023-03-29 09:15:37 -0500

    added composer lock files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b03b76a6287d0c2423e0ded950f0afab9730a3fc',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b03b76a6287d0c2423e0ded950f0afab9730a3fc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
